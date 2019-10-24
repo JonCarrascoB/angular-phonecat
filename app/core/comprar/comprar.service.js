bcbad0f5ebeba2fc386e01575ef64ba13e537b0f
Jon Carrasco <jcarrascobar@gmail.com>
2019-10-24 19:47:41 +0200

    carrito contador inicio

--- app/core/comprar/comprar.service.js
+++ app/core/comprar/comprar.service.js
@@ -4,12 +4,20 @@ core.factory('compraMovil',
     function() {
       
       return {
+        
         producto: [],
+        cantidad: 0,
         getProducto: function() {
-            return this.producto;
+            return (this.producto && this.cantidad);
         },
         setProducto: function(movil) {
+          if(this.producto.get(movil.id)){
+            this.cantidad = this.cantidad + 1;
+          } else {
             this.producto.push(movil);
+            this.cantidad = 1;
+          }
+           
         }
 
       }; //end return
